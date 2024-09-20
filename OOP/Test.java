b5fc8dff515965fd8c29149481c1d5a5c036ae70
Adithyasaju <adithyasajuo7oaaa2gmail.com>
2024-09-20 19:20:09 +0530

    learn

---
+++ OOP/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Account a1= new Account();
+        Account a2= new Account();
+
+        a1.setAcc_Id(101);
+        a2.setAcc_Name("Sonia");
+        System.out.println("Account Id:"+a1.getAcc_Id());
+        System.out.println("Account Name:"+a2.getAcc_Name());
+            
+        
+    }
+    
+}
